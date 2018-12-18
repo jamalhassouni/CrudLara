8d2e4b692fa0079404f97523adf0a26072dd956f
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-18 19:39:12 +0100

    Create About Component

---
+++ resources/assets/js/components/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+/**
+ * About
+ */
+class About extends Component {
+  // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return <div>MY About COMPONENT</div>;
+  }
+}
+
+About.propTypes = {
+  //  prop: PropTypes.type.isRequired
+};
+
+export default About;
