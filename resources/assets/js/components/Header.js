409ee2d484e619e43161c45f6ad4e38842d7af87
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-18 19:38:49 +0100

    chore:Install React Router Package

--- resources/assets/js/components/Header.js
+++ resources/assets/js/components/Header.js
@@ -1,12 +1,22 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+/**
+ * Header
+ */
 export default class Header extends Component {
-    render() {
-        return (
-         <div>
-         this is  header
-         </div>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Link to="/"> Home </Link>
+          <Link to="/about"> About Us </Link>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </div>
+      </Router>
+    );
+  }
 }
--- resources/assets/js/components/Index.js
+++ resources/assets/js/components/Index.js
@@ -1,28 +1,22 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header';
-import Footer from './Footer';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
+/**
+ * Index
+ */
 export default class Index extends Component {
-    render() {
-        return (<div className="container">
-          <Header/>
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Landing Component</div>
-
-                        <div className="card-body">
-                            I'm an landing component!
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Footer/>
-        </div>);
-    }
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
 }
 
-if (document.getElementById('app')) {
-    ReactDOM.render(<Index/>, document.getElementById('app'));
+if (document.getElementById("app")) {
+  ReactDOM.render(<Index />, document.getElementById("app"));
 }
