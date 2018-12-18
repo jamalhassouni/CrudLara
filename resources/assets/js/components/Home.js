b6261566272bfdf6d35eaabaa080c5bd54c0e4e0
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-18 19:39:27 +0100

    Create Home Component

---
+++ resources/assets/js/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * Home
+ */
+class Home extends React.Component {
+  render() {
+    return <div>MY Home COMPONENT</div>;
+  }
+}
+
+export default Home;
